33037dc552c8f7c5572c2d9e1ab8543b17367411
IzabelaMalinowska <114566540+IzabelaMalinowska@users.noreply.github.com>
2023-07-12 15:59:47 +0200

    update

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,11 +2,13 @@ import { useSearchParams } from 'react-router-dom';
 import * as API from '../../utils/api/apiMoviedb';
 import { useEffect, useState } from 'react';
 import { TrendingList } from 'components/TrendingList/TrendingList';
+import NotFound  from '../../pages/NotFound';
 
 
 export const SearchForm = () => {
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [notFound, setNotFound] = useState(false);
 
   const handleSubmit = e => {
     setSearchParams({ query: e.target[0].value });
@@ -17,14 +19,30 @@ export const SearchForm = () => {
   useEffect(() => {
     const movieTitle = searchParams.get('query');
     if (movieTitle) {
-      API.searchMovies(movieTitle).then(ret => setMovies(ret));
+      API.searchMovies(movieTitle)
+        .then(ret => {
+          if (ret.length === 0) {
+            setNotFound(true);
+          } else {
+            setMovies(ret);
+            setNotFound(false);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          setNotFound(true);
+        });
     }
   }, [searchParams]);
+
+  if (notFound) {
+    return <NotFound />;
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit} style={{ margin: '15px' }}>
-        <input type="text" placeholder="Search..."></input>
+        <input type="text" placeholder="Search..." />
         <button type="submit">Search</button>
       </form>
       <TrendingList movies={movies} />
